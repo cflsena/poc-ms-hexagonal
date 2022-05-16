4f0be7ee1a63a55cf1a48c9ccb58daae79dbadaf
Cleiton Sena <cflsena2@gmail.com>
2022-05-15 23:05:43 -0300

    Adicionando application yml somente na camada de adapters

--- contract/src/main/java/poc/ms/hexagonal/config/ModulePropertiesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package poc.ms.hexagonal.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(value = "classpath:contract_config.properties")
-public class ModulePropertiesConfiguration {
-}
